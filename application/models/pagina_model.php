2f0c761a24a41a71655b780e00075d183008a91f
Rogier Neeleman <rogier@jota-joti.nl>
2013-01-13 13:53:06 +0100

    Model voor ophalen pagina's gemaakt.

---
+++ application/models/pagina_model.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Pagina_model extends CI_Model{
+        
+		public function get_pagina($pagina){
+			$this->db->where('urlnaam', $pagina);
+
+			$query = $this->db->get('pagina');
+			return $query->row();
+		}
+		
+    }
