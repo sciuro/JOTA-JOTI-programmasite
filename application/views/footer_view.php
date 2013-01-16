5176d17c19e223367dc6a19e6d062ce6a7e4b12c
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-01-16 22:49:34 +0100

    Opmaak van de algemene pagina's leesbaarder gemaakt.

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -1,6 +1,9 @@
-<footer>
-<p>&copy; 2013 Scouting Nederland. All rights reserved.</p>
-</footer>
+<div class="container">
+	<hr>
+	<footer>
+		<p>&copy; 2013 Scouting Nederland. All rights reserved.</p>
+	</footer>
+</div>
 
 <script src="<?php echo base_url();?>assets/jquery/jquery.js"></script>
 <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.js"></script>
--- application/views/pagina_view.php
+++ application/views/pagina_view.php
@@ -1,3 +1,19 @@
-<?php echo $pagina['tekst']; ?>
+<div class='container'>
+	<header class="jumbotron subhead">
+		<div class="container">
+			<h1><?php echo $pagina['titel']; ?></h1>
+		</div>
+	</header>
 
-<p>Laatste aanpassing: <?php echo $pagina['timestamp']; ?>
+	<div class='row'>
+		<div class='span12'>
+			<?php echo $pagina['tekst']; ?>
+		</div>
+	</div>
+
+	<div class='row'>
+		<div class='span4 offset8'>
+			<small>Laatste aanpassing: <?php echo $pagina['timestamp']; ?></small>
+		</div>
+	</div>
+</div>
