7d7dbc83682a7d2b9ddbcab45048e6863b0f50a9
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-01-16 23:54:01 +0100

    Foutje in menu aangepast

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -36,11 +36,11 @@
 						<ul class="dropdown-menu" role="menu">
 							<li><a tabindex="-1" href="<?php echo base_url();?>info/pagina/spelen">Algemeen</a></li>
                             <li class="divider"></li>
-							<li><a tabindex="-1" href="<?php echo base_url();?>spelen/speltak/bevers/">Bevers</a></li>
-                            <li><a tabindex="-1" href="<?php echo base_url();?>spelen/speltak/welpen/">Welpen</a></li>
-                            <li><a tabindex="-1" href="<?php echo base_url();?>spelen/speltak/scouts/">Scouts</a></li>
-                            <li><a tabindex="-1" href="<?php echo base_url();?>spelen/speltak/explorers/">Explorers</a></li>
-                            <li><a tabindex="-1" href="<?php echo base_url();?>spelen/speltak/roverscouts/">Roverscouts</a></li>
+							<li><a tabindex="-1" href="<?php echo base_url();?>overzicht/speltak/bevers/">Bevers</a></li>
+                            <li><a tabindex="-1" href="<?php echo base_url();?>overzicht/speltak/welpen/">Welpen</a></li>
+                            <li><a tabindex="-1" href="<?php echo base_url();?>overzicht/speltak/scouts/">Scouts</a></li>
+                            <li><a tabindex="-1" href="<?php echo base_url();?>overzicht/speltak/explorers/">Explorers</a></li>
+                            <li><a tabindex="-1" href="<?php echo base_url();?>overzicht/speltak/roverscouts/">Roverscouts</a></li>
 
 						</ul>
 					</li>
