cf09456f8c8a8784785ce0f5b471c23ad11a8cce
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-01-19 13:33:26 +0100

    Voorbeeld verwijderd

--- application/views/beheer_view.php
+++ application/views/beheer_view.php
@@ -272,45 +272,6 @@
     <?php } elseif ($tab == "duur") { ?>
     <h3>Totale programmaduur</h3>
 
-	
-        <?php $attributes = array('class' => 'form-horizontal');
-    echo form_open('beheer/save/'.$tab, $attributes); ?>
-    </form>
-
-   		<div class="control-group">
-   			<label class="control-label" for="proxyhost">Proxy servername:</label>
-   			<div class="controls">
-   				<input type="text" id="proxyhost" name="proxyhost" placeholder="Proxy servername" value="<?php echo $setting['proxyhost']; ?>">
-   				<input type="text" id="proxyhost" name="proxyport" class="input-small" placeholder="8080" value="<?php echo $setting['proxyport']; ?>">
-   			</div>
-   		</div>
-
-   		<div class="control-group">
-   			<label class="control-label" for="http">Proxy type:</label>
-   			<div class="controls">
-   				<?php if ($setting['proxytype'] == "http") {$checked = 'checked';} else {$checked=''; } ?>
-   				<label class="radio"><input type="radio" name="proxytype" value="http" <?php echo $checked; ?>>HTTP</label>
-   				<?php if ($setting['proxytype'] == "socks5") {$checked = 'checked';} else {$checked=''; } ?>
-   				<label class="radio"><input type="radio" name="proxytype" value="socks5" <?php echo $checked; ?>>Socks5</label>
-   			</div>
-   		</div>
-
-   		<div class="control-group">
-   			<label class="control-label" for="proxyauth">Proxy authentication:</label>
-   			<div class="controls">
-   				<input type="text" id="proxyauth" name="proxyuser" class="input-small" placeholder="Username"  value="<?php echo $setting['proxyuser']; ?>">
-    			<input type="password" id="proxyauth" name="proxypassword" class="input-small" placeholder="Password" value="<?php echo $setting['proxypassword']; ?>">
-   			</div>
-   		</div>
-
-   		<div class="control-group">
-   			<div class="controls">
-   				<button type="submit" class="btn">Submit</button>
-   			</div>
-   		</div>
-
-    </form>
-
     <?php } ?>
 
 </div>
