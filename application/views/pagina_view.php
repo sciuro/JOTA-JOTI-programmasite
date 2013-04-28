6630e556169d298d4ffcea955440956f45aa3828
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-04-28 14:16:27 +0200

    Carousel bijgeplaatst

--- application/views/pagina_view.php
+++ application/views/pagina_view.php
@@ -1,4 +1,23 @@
 <div class='container pagina'>
+<div class='row'>
+    <div id="myCarousel" class="span12 carousel slide">
+
+    	<!-- Carousel items -->
+    	<div class="carousel-inner">
+    		<div class="item active">
+    			<center><img src="<?php echo base_url();?>images/carousel_1.png"></center>
+      		</div>
+    		<div class="item">
+    			<center><img src="<?php echo base_url();?>images/carousel_2.png"></center>
+    		</div>
+    	</div>
+
+    	<!-- Carousel nav -->
+    	<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+    	<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div>
+</div>
+
 	<header class="jumbotron subhead">
 			<h1><?php echo $pagina['titel']; ?></h1>
 	</header>
