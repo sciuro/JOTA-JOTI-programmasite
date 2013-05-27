5054432ac44d8d33731c366f82777ef76ea47b9d
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-05-28 00:35:45 +0200

    Plaatjes carrousel draaide niet automatisch

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -7,5 +7,15 @@
 
 <script src="<?php echo base_url();?>assets/jquery/jquery.js"></script>
 <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.js"></script>
+<?php if (isset($pagina['banner'])) {
+if ($pagina['banner'] == 1) { ?>
+<script>
+      $(document).ready(function(){
+        $('.carousel').carousel({
+          interval: 4000
+        });
+      });
+</script>
+<?php } } ?>
 </body>
 </html>
