b84d115ce5ddaf013d21a93536133c7ba6c77972
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-06-01 00:56:17 +0200

    div aanpassing speltak pagina

--- application/views/spellen_keuze_view.php
+++ application/views/spellen_keuze_view.php
@@ -9,46 +9,42 @@
 
 	<div class='row-fluid'>
 		<div class='span8 offset1'>
-			<h4>Online spelen Jota & Joti:</h4>
-			<ul>
-				<?php foreach ($duur as $opkomsttijd) { ?>
-					<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/web/<?php echo $opkomsttijd['lengte'];?>">Totale opkomstduur van <?php echo $opkomsttijd['lengte'];?> uur.</a></li>
-				<?php }?>
-			</ul>
-		</div>
-	</div>
+			<div>
+				<h4>Online spelen Jota & Joti:</h4>
+				<ul>
+					<?php foreach ($duur as $opkomsttijd) { ?>
+						<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/web/<?php echo $opkomsttijd['lengte'];?>">Totale opkomstduur van <?php echo $opkomsttijd['lengte'];?> uur.</a></li>
+					<?php }?>
+				</ul>
+			</div>
 
-	<div class='row-fluid'>
-		<div class='span8 offset1'>
-			<h4>Online spelen Jota:</h4>
-			<ul>
-				<?php foreach ($duur as $opkomsttijd) { ?>
-					<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/web/<?php echo $opkomsttijd['lengte'];?>/jota">Totale opkomstduur van <?php echo $opkomsttijd['lengte'];?> uur.</a></li>
-				<?php }?>
-			</ul>
-		</div>
-	</div>
+			<div>
+				<h4>Online spelen Jota:</h4>
+				<ul>
+					<?php foreach ($duur as $opkomsttijd) { ?>
+						<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/web/<?php echo $opkomsttijd['lengte'];?>/jota">Totale opkomstduur van <?php echo $opkomsttijd['lengte'];?> uur.</a></li>
+					<?php }?>
+				</ul>
+			</div>
 
-	<div class='row-fluid'>
-		<div class='span8 offset1'>
-			<h4>Online spelen Joti:</h4>
-			<ul>
-				<?php foreach ($duur as $opkomsttijd) { ?>
-					<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/web/<?php echo $opkomsttijd['lengte'];?>/joti">Totale opkomstduur van <?php echo $opkomsttijd['lengte'];?> uur.</a></li>
-				<?php }?>
-			</ul>
-		</div>
-	</div>
+			<div>
+				<h4>Online spelen Joti:</h4>
+				<ul>
+					<?php foreach ($duur as $opkomsttijd) { ?>
+						<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/web/<?php echo $opkomsttijd['lengte'];?>/joti">Totale opkomstduur van <?php echo $opkomsttijd['lengte'];?> uur.</a></li>
+					<?php }?>
+				</ul>
+			</div>
 
-	<div class='row-fluid'>
-		<div class='span8 offset1'>
-			<h4>Spelen downloaden (pdf) voor offline gebruikt:</h4>
-			<ul>
-				<?php foreach ($duur as $opkomsttijd) { ?>
-					<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/pdf/<?php echo $opkomsttijd['lengte'];?>">Totale opkomstduur van <?php echo $opkomsttijd['lengte']?> uur.</a></li>
-				<?php }?>
-			</ul>
+			<div>
+				<h4>Spelen downloaden (pdf) voor offline gebruikt:</h4>
+				<ul>
+					<?php foreach ($duur as $opkomsttijd) { ?>
+						<li><a href="<?php echo base_url();?>spellen/<?php echo $speltak;?>/pdf/<?php echo $opkomsttijd['lengte'];?>">Totale opkomstduur van <?php echo $opkomsttijd['lengte']?> uur.</a></li>
+					<?php }?>
+				</ul>
+			</div>
+			
 		</div>
-	</div>
 
 </div>
