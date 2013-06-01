c9b27739df9d98c59b1e7f805dac2dbd67c4f22f
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-06-02 01:24:59 +0200

    Favicon naar SN standaard

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -21,7 +21,7 @@
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url();?>assets/ico/apple-touch-icon-114-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url();?>assets/ico/apple-touch-icon-72-precomposed.png">
         <link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>assets/ico/apple-touch-icon-57-precomposed.png">
-        <link rel="shortcut icon" href="<?php echo base_url();?>assets/ico/favicon.png">
+        <link rel="shortcut icon" href="<?php echo base_url();?>assets/ico/favicon.ico">
 
     </head>
 
