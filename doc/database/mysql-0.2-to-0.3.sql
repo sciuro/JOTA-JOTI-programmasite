f99d1e248e3e8f86056d3d46782c0fef61231278
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-06-03 22:12:27 +0200

    Documentatie bijgewerkt

--- doc/database/mysql-0.2-to-0.3.sql
+++ doc/database/mysql-0.2-to-0.3.sql
@@ -1,1 +1,14 @@
+
 -- Upgradescript database van versie 0.2 naar 0.3
+
+-- Aanmaken usertabel
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `voornaam` varchar(64) NOT NULL,
+  `achternaam` varchar(64) NOT NULL,
+  `rol` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
