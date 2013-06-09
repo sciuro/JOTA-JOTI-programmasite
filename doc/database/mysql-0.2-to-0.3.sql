8fd891953fc5d767548db6750a88a7d96605bc75
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-06-09 19:58:24 +0200

    Tabellen voor subgroepen en scores

--- doc/database/mysql-0.2-to-0.3.sql
+++ doc/database/mysql-0.2-to-0.3.sql
@@ -47,3 +47,23 @@ ALTER TABLE `groep` DROP `wachtwoord`;
 
 -- Groepsdata word nu ook opgeslagen
 ALTER TABLE `statistics` ADD `group` INT( 6 ) NOT NULL AFTER `user`;
+
+-- Scoutsspel
+CREATE TABLE IF NOT EXISTS `subgroep` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `groepid` int(6) NOT NULL,
+  `naam` varchar(64) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `score`;
+CREATE TABLE IF NOT EXISTS `score` (
+  `spel_id` int(4) NOT NULL,
+  `groep_id` int(6) NOT NULL,
+  `subgroep_id` int(4) NOT NULL,
+  `gespeeld` tinyint(1) NOT NULL DEFAULT '0',
+  `punten` int(4) NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `spel_id` (`spel_id`,`groep_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
