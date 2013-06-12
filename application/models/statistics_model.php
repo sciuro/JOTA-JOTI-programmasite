5f3f4cfafabf8020886658536c3edb0c544d376d
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-06-12 21:15:30 +0200

    Foutje in model

--- application/models/statistics_model.php
+++ application/models/statistics_model.php
@@ -3,18 +3,6 @@
     class Statistics_model extends CI_Model{
 
 		public function get_hits($from, $to) {
-			SELECT CONCAT(
-    YEAR(timestamp),
-    MONTH(timestamp),
-    DAY(timestamp),
-    HOUR(timestamp),
-    COUNT(*)
-FROM `statistics`
-GROUP BY
-    YEAR(timestamp),
-    MONTH(timestamp),
-    DAY(timestamp),
-    HOUR(timestamp)
     
 			return;
 		}
