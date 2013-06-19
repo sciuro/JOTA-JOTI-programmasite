83be841522807fb03a3997349c8a81f7ec87bc38
netforce <netforce@scoutlink.net>
2013-06-19 09:41:59 +0200

    Textuele aanpasingen

--- application/views/login_persoon_view.php
+++ application/views/login_persoon_view.php
@@ -11,7 +11,7 @@
 		<div class='span8 offset1'>
 			<p>Hieronder is het mogelijk in te loggen voor individuele personen welke hier expliciet toestemming voor
 				hebben gekregen van de JOTA-JOTI organisatie. Indien je dat niet hebt, heb vooral veel plezier met de
-				rest van de website en de mogelijkheden welke deze bied.</p>
+				rest van de website en de mogelijkheden welke deze biedt.</p>
 			<p>Indien je van mening bent dat je een account zou moeten hebben, dan weet je ook waar je deze moet aanvragen.</p>
 			<p>If you do not understand the Dutch text above, please visit the 
 				<a href='http://www.scout.org/en/information_events/events/jota'>international scouts website</a>.</p>
@@ -30,14 +30,14 @@
 			<?php } ?>
 
 			<div class="control-group">
-				<label class="control-label" for="email"><em>E-Mail</em></label>
+				<label class="control-label" for="email"><em>E-mailadres</em></label>
 				<div class="controls">
 					<input type='text' name='email' id='email' size='25'>
 				</div>
 			</div>
 
 			<div class="control-group">
-				<label class="control-label" for="password"><em>wachtwoord</em></label>
+				<label class="control-label" for="password"><em>Wachtwoord</em></label>
 				<div class="controls">
 					<input type='password' name='password' id='password' size='25'>
 				</div>
