dc3d6e9141246a85bee89377322eedace98c4095
Rogier Neeleman <rogier.neeleman@jota-joti.nl>
2013-09-12 19:29:30 +0200

    De scouts hun speltakplaat is toegevoegd.

--- doc/database/mysql-0.2-to-0.3.sql
+++ doc/database/mysql-0.2-to-0.3.sql
@@ -104,3 +104,12 @@ INSERT INTO `wincode` (`code`, `youtube`, `plaatje`) VALUES
 ('scouts-1-2-3-4-5-6-7-8', '', 'http://someonenoticed.files.wordpress.com/2011/03/thumbsupd.png'),
 ('bevers-8-7-6-5-4-3-2-1', 'Ld1DTmXesTo', '');
 
+-- De speltakpagina van de scouts werkt nu ook.
+UPDATE `jota`.`gebied` SET `kaartloc` = '65,130,179,334' WHERE `gebied`.`id` =35;
+UPDATE `jota`.`gebied` SET `kaartloc` = '297,124,411,316' WHERE `gebied`.`id` =36;
+UPDATE `jota`.`gebied` SET `kaartloc` = '410,126,524,318' WHERE `gebied`.`id` =37;
+UPDATE `jota`.`gebied` SET `kaartloc` = '180,125,294,317' WHERE `gebied`.`id` =38;
+UPDATE `jota`.`gebied` SET `kaartloc` = '62,342,176,534' WHERE `gebied`.`id` =39;
+UPDATE `jota`.`gebied` SET `kaartloc` = '298,339,412,531' WHERE `gebied`.`id` =40;
+UPDATE `jota`.`gebied` SET `kaartloc` = '174,326,288,535' WHERE `gebied`.`id` =41;
+UPDATE `jota`.`gebied` SET `kaartloc` = '415,339,529,531' WHERE `gebied`.`id` =42;
